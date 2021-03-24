faadc2564174151dd6dbfd5e4c8c08b928f779ab
fdd <furuoxuan@ssit.cc>
2021-03-24 17:31:49 +0800

    login

---
+++ app/src/main/java/com/frx/jetpro/ui/login/LoginActivity.java
@@ -0,0 +1,19 @@
+package com.frx.jetpro.ui.login;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.frx.jetpro.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_layout_login);
+
+    }
+
+}
