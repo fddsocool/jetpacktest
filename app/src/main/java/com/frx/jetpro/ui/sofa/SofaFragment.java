bc97bfc9de88fab2186568a6f8d441d6a987ef85
fdd <furuoxuan@ssit.cc>
2021-04-09 17:21:38 +0800

    提交 沙发页面布局

--- app/src/main/java/com/frx/jetpro/ui/sofa/SofaFragment.java
+++ app/src/main/java/com/frx/jetpro/ui/sofa/SofaFragment.java
@@ -1,28 +1,24 @@
 package com.frx.jetpro.ui.sofa;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-import com.elvishew.xlog.XLog;
-import com.frx.jetpro.R;
+import com.frx.jetpro.databinding.FragmentSofaBinding;
 import com.frx.libnavannotation.FragmentDestination;
 
 @FragmentDestination(pageUrl = "main/tabs/sofa")
 public class SofaFragment extends Fragment {
 
+    private FragmentSofaBinding fragmentSofaBinding;
+
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_sofa, container, false);
-        final TextView textView = root.findViewById(R.id.text_sofa);
-        textView.setText("sofa");
-        return root;
+        fragmentSofaBinding = FragmentSofaBinding.inflate(inflater, container, false);
+        return fragmentSofaBinding.getRoot();
     }
 }
